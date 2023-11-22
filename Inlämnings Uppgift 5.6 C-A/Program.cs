52f28665cd83f2fcad9b96ecdd030eeef5b81eb4
sere06001 <sere06001@utb.vaxjo.se>
2023-11-22 17:32:24 +0100

    Add project files.

---
+++ Inlämnings Uppgift 5.6 C-A/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift5_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en rad av tal separerade med stjärnor, '*'.");
+            string[] allaTal = Console.ReadLine().Split("*");
+            long produkt = 1;
+            for (int i = 0; i < allaTal.Length; i++)
+            {
+                produkt = produkt*long.Parse(allaTal[i]);
+            }
+            Console.WriteLine("Produkten: "+produkt);
+            Console.ReadKey();
+        }
+    }
+}
